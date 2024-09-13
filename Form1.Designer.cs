27a328c844e15c9874a3fe69fd1b826b3bb89f1b
Moises-beeo <moises.chavez1805@gmail.com>
2024-09-12 23:16:58 -0600

    inicio

---
+++ Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace UnIdad2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtSalario = new System.Windows.Forms.TextBox();
+            this.txtAños = new System.Windows.Forms.TextBox();
+            this.cbxCategoría = new System.Windows.Forms.ComboBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(458, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "INGRESE SU NOMBRE, SALARIO, AÑOS TRABAJADOS Y SU CATEGORÍA DE EMPLEADO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 85);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Salario";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Años Trabajados (En número)";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(55, 201);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(104, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Categoría Empleado";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(243, 82);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(149, 20);
+            this.txtNombre.TabIndex = 5;
+            // 
+            // txtSalario
+            // 
+            this.txtSalario.Location = new System.Drawing.Point(243, 113);
+            this.txtSalario.Name = "txtSalario";
+            this.txtSalario.Size = new System.Drawing.Size(149, 20);
+            this.txtSalario.TabIndex = 6;
+            // 
+            // txtAños
+            // 
+            this.txtAños.Location = new System.Drawing.Point(243, 156);
+            this.txtAños.Name = "txtAños";
+            this.txtAños.Size = new System.Drawing.Size(149, 20);
+            this.txtAños.TabIndex = 7;
+            // 
+            // cbxCategoría
+            // 
+            this.cbxCategoría.FormattingEnabled = true;
+            this.cbxCategoría.Items.AddRange(new object[] {
+            "Categoría 1",
+            "Categoría 2",
+            "Categoría 3"});
+            this.cbxCategoría.Location = new System.Drawing.Point(243, 193);
+            this.cbxCategoría.Name = "cbxCategoría";
+            this.cbxCategoría.Size = new System.Drawing.Size(149, 21);
+            this.cbxCategoría.TabIndex = 8;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(29, 259);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(215, 259);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(390, 259);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 11;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(498, 324);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.cbxCategoría);
+            this.Controls.Add(this.txtAños);
+            this.Controls.Add(this.txtSalario);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TRABAJO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtSalario;
+        private System.Windows.Forms.TextBox txtAños;
+        private System.Windows.Forms.ComboBox cbxCategoría;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnSalir;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnIdad2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string nombre, caEmpleado;
+            int categoria;
+            double salario, salarioTotal, añosTrabajados, incrementoCa = 0, incrementoAño, incremento;
+
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                MessageBox.Show("El campo nombre no está llenado");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtAños.Text))
+            {
+                MessageBox.Show("El campo años trabajados no está llenado");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(txtSalario.Text))
+            {
+                MessageBox.Show("El campo salario no está llenado");
+                return;
+            }
+
+            if (cbxCategoría.SelectedIndex == -1)
+            {
+                MessageBox.Show("No eligio su categoría");
+                return;
+            }
+
+            if (double.TryParse(txtSalario.Text, out salario ) == false || double.Parse(txtSalario.Text) == 0)
+            {
+                MessageBox.Show("Por favor ingrese un valor de salario válido.");
+                txtSalario.Text = "";
+                txtSalario.Focus();
+                return;
+            }
+
+            if (double.TryParse(txtAños.Text, out añosTrabajados) == false || double.Parse(txtSalario.Text) == 0)
+            {
+                MessageBox.Show("Por favor ingrese un valor de año válido.");
+                txtAños.Text = "";
+                txtAños.Focus();
+                return;
+            }
+
+            nombre = txtNombre.Text;
+            caEmpleado = Convert.ToString(cbxCategoría.SelectedItem);
+            salario = Convert.ToDouble(txtSalario.Text);
+            añosTrabajados = Convert.ToDouble(txtAños.Text);
+
+            categoria = cbxCategoría.SelectedIndex;
+
+            switch (categoria)
+            {
+                case 0: 
+                    incrementoCa = 0.15;
+                    break;
+                case 1:
+                    incrementoCa = 0.10;
+
+                    break;
+                case 2:
+                    incrementoCa = 0.05;
+                    break;
+                deafault: break;
+            }
+
+
+            incrementoAño = 0.05 * añosTrabajados;
+            incremento = incrementoCa + incrementoAño;
+
+            salarioTotal = salario + (salario * incremento);
+
+            MessageBox.Show("El empleado: " + nombre  + "\nObtendra este salario el próximo año: $" + salarioTotal);
+
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtAños.Text = "";
+            txtNombre.Text = "";
+            txtSalario.Text = "";
+            cbxCategoría.SelectedIndex = -1;
+            txtNombre.Focus();
+        }
+
+       
+    }
+}
